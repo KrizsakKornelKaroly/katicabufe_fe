ffc7cc85195c7af6c9bd05fc8c213b1327b96c28
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-10-07 12:25:41 +0200

    form felv. start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CategoryListComponent } from './components/categories/list/list.component';
 import { TrafficListComponent } from './components/traffic/list/list.component';
+import { TrafficFormComponent } from './components/traffic/form/form.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,10 @@ export const routes: Routes = [
     {
         path: 'traffic',
         component: TrafficListComponent
+    },
+    {
+        path: 'trafficform',
+        component: TrafficFormComponent
     }
        
 ];
--- src/app/components/traffic/form/form.component.ts
+++ src/app/components/traffic/form/form.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-form',
+  selector: 'app-trafficForm',
   standalone: true,
   imports: [],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class TrafficFormComponent {
 
 }
