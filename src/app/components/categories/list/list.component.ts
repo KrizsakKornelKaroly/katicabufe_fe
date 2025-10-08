8e31ca9cba686d8bc47b7aa5d366a6cd6e6dcb35
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-10-08 12:11:30 +0200

    API call traffic + categories

--- src/app/components/categories/list/list.component.ts
+++ src/app/components/categories/list/list.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import axios from 'axios';
+
+interface Category {
+  id: number;
+  kategoriaNev: string;
+}
+
 
 @Component({
   selector: 'app-categoryList',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class CategoryListComponent {
 
+export class CategoryListComponent implements OnInit {
+
+  categories: Category[] = [];
+
+  async ngOnInit() {
+    try {
+      const response = await axios.get('http://localhost:3000/categories');
+      this.categories = response.data;
+
+    } catch (error: any ) {
+      console.log(error.message);
+      alert('Hiba a kategóriák betöltése során');
+    }
+
+  }
 }
--- src/app/components/traffic/list/list.component.ts
+++ src/app/components/traffic/list/list.component.ts
@@ -1,13 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import axios from 'axios';
+
+interface Traffic {
+  id: number;
+  termek: string;
+  vevo: string;
+  kategoriaId: number;
+  egyseg : string;
+  nettoar: number;
+  mennyiseg : number;
+  kiadva : boolean;
+}
 
 @Component({
   selector: 'app-trafficList',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class TrafficListComponent {
+
+  traffic: Traffic[] = [];
+
+  async ngOnInit() {
+    try {
+      const response = await axios.get('http://localhost:3000/traffic');
+      this.traffic = response.data;
+      console.log(this.traffic);
+
+    } catch (error: any ) {
+      console.log(error.message);
+      alert('Hiba a kategóriák betöltése során');
+    }
+
+  }
 
 }
