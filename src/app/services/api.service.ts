3a7f6e2d81d39f831462db3ae5de772a8bf43256
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-10-14 13:19:01 +0200

    api service: full comm. (select, update, insert, delete)

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
-import { ApiResponse} from '../interfaces/apires';
+import { ApiResponse } from '../interfaces/apires';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class ApiService {
   constructor() { }
 
 
-  async selectAll(table: string) : Promise<ApiResponse> {
+  async selectAll(table: string): Promise<ApiResponse> {
     try {
       const response = await axios.get(`${this.SERVER}/${table}`);
       return {
@@ -19,25 +19,111 @@ export class ApiService {
         data: response.data
       };
 
-    } 
-    catch (error: any) 
-    {
+    }
+    catch (error: any) {
       console.log(error.message);
       return {
         status: 500,
-        message: 'Hiba történt az adatok elérésekor!' 
+        message: 'Hiba történt az adatok elérésekor!'
       };
     }
 
   }
 
-  select() { }
+  async select(table: string, id: number): Promise<ApiResponse> {
 
-  insert() { }
+    try {
+      const response = await axios.get(`${this.SERVER}/${table}/${id}`);
+      return {
+        status: 200,
+        data: response.data
+      };
 
-  update() { }
+    }
+    catch (error: any) {
+      console.log(error.message);
+      return {
+        status: 500,
+        message: 'Hiba történt az adatok elérésekor!'
+      };
+    }
 
-  delete() { }
+  }
 
-  deleteAll() { }
+  async insert(table: string, data: any) {
+    try {
+      const response = await axios.post(`${this.SERVER}/${table}`, data);
+      return {
+        status: 200,
+        message: "Rekord hozzáadva!",
+        data: response.data  //nem kötelező visszaadni
+      };
+
+    }
+    catch (error: any) {
+      console.log(error.message);
+      return {
+        status: 500,
+        message: 'Hiba történt a művelet során!'
+      };
+    }
+  }
+
+  async update(table: string, id: number, data: any) {
+    try {
+      const response = await axios.patch(`${this.SERVER}/${table}`, data);
+      return {
+        status: 200,
+        message: "Rekord módosítva!",
+        data: response.data  //nem kötelező visszaadni
+      };
+
+    }
+    catch (error: any) {
+      console.log(error.message);
+      return {
+        status: 500,
+        message: 'Hiba történt a művelet során!'
+      };
+    }
+
+  }
+
+  async delete(table: string, id: number) {
+
+    try {
+      const response = await axios.delete(`${this.SERVER}/${table}/${id}`);
+      return {
+        status: 200,
+        message: "Rekord törölve!"
+      };
+
+    }
+    catch (error: any) {
+      console.log(error.message);
+      return {
+        status: 500,
+        message: 'Hiba történt az adatok elérésekor!'
+      };
+    }
+
+
+  }
+
+  async deleteAll(table: string) {
+    try {
+      const response = await axios.delete(`${this.SERVER}/${table}`);
+      return {
+        status: 200,
+        message: "Összes rekord törölve a táblából!"
+      };
+
+    } catch (error: any) {
+      console.log(error.message);
+      return {
+        status: 500,
+        message: 'Hiba történt az adatok elérésekor!'
+      };
+    }
+  }
 }
